80c69c187ec7152b5b2476b2cac6e303a86ecde2
Alexander <1@192.168.0.59>
2013-11-11 23:12:51 +0400

    Вариант №31.Приёмная Комиссия.

---
+++ JavaApplication44/src/javaapplication44/JavaApplication44.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication44;
+
+/**
+ *
+ * @author 1
+ */
+public class JavaApplication44 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
